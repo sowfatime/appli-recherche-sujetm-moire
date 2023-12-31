0360e423cca7051b38e12404bac2258704f6a058
saloum45 <saloumfall45@gmail.com>
2023-12-31 20:19:46 +0000

    in progress

--- src/admin/domaine/Domaine.js
+++ src/admin/domaine/Domaine.js
@@ -13,9 +13,11 @@ import {
 import { db } from "../../config/config";
 function Domaine() {
   const [domaine, setDomaine] = useState([]);
+  let champs = document.getElementById("inputValue");
+  let valueForAddUpdate=0;
+  let currentId=0;
 
   const domaineAdd = async () => {
-    let champs = document.getElementById("inputValue");
     try {
       if (champs.value === "") {
         alert("champs vide");
@@ -33,6 +35,28 @@ function Domaine() {
     domaineRead();
   };
 
+  const domaineEdit = async (id) => {
+    currentId=id;
+    valueForAddUpdate=1;
+    // const selectedDomaine = doc(db, "domaine", id);
+    const docRef = doc(db, "domaine", id);
+    const selectedDomaine = await getDoc(docRef);
+    
+    console.log("Document data:", selectedDomaine.data());
+    champs.value=selectedDomaine.data().name;
+  };
+  const domaineUpdate = async (id) => {
+    const selectedDomaine = doc(db, "domaine", id);
+
+    const newData = {
+      name: champs.value,
+    };
+
+    await updateDoc(selectedDomaine, newData);
+
+    domaineRead();
+  };
+
   const domaineRead = async () => {
     const domaineArray = [];
     const querySnapshot = await getDocs(collection(db, "domaine"));
@@ -44,14 +68,28 @@ function Domaine() {
     setDomaine(domaineArray);
   };
   const domaineDelete = async (id) => {
+    // let isForDelet=confirm("etes vous sûr");
+    // if (is) {
+
+    // }
     const selectedUser = doc(db, "domaine", id);
 
     await deleteDoc(selectedUser);
 
     domaineRead();
-    alert('suppression faite avec succès')
+    alert("suppression faite avec succès");
   };
+
+  const verifFunctionToCall=()=>{
+    if (valueForAddUpdate===0) {
+      domaineAdd();
+    }else if (valueForAddUpdate===1) {
+      domaineUpdate(currentId);
+    }
+  };
+
   domaineRead();
+
   return (
     <>
       <div className="w-50 m-auto mt-1">
@@ -66,7 +104,7 @@ function Domaine() {
             className="w-100"
           />
         </div>
-        <button className="btn btn-primary w-100" onClick={domaineAdd}>
+        <button className="btn btn-primary w-100" onClick={verifFunctionToCall}>
           send
         </button>
       </div>
@@ -84,8 +122,18 @@ function Domaine() {
                   <p className="card-text">{dom.name}</p>
                 </div>
                 <div className="w-25 d-flex justify-content-between pb-1 px-1">
-                  <i className="bi bi-trash3-fill" onClick={()=>{domaineDelete(dom.id)}} ></i>
-                  <i className="bi bi-pencil-square"></i>
+                  <i
+                    className="bi bi-trash3-fill"
+                    onClick={() => {
+                      domaineDelete(dom.id);
+                    }}
+                  ></i>
+                  <i
+                    className="bi bi-pencil-square"
+                    onClick={() => {
+                      domaineEdit(dom.id);
+                    }}
+                  ></i>
                 </div>
               </div>
             </div>
